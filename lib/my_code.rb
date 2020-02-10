fb3c91118ac4b81062ce6a9487f7d130c14dd873
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-10 22:04:09 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+  noo_array = []
+  i = 0
+  while i < array.length
+    noo_array << (yield(array[i]))
+    i += 1
+  end
+  noo_array
+end
+
+def reduce(array, sv=nil)
+  if sv
+    sum = sv
+    i = 0 
+  else 
+    sum = array[0]
+    i = 1 
+  end
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1
+  end
+  sum
+end
